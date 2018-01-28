32eea72fedb08aa9d90b66abc8a6ab5b1bb7d7c3
TowerLoong <towerloong@hotmail.com>
2018-01-28 18:05:38 +0800

    新建了一个工程

---
+++ src/main/java/com/hi/HiStudyBarPro/HiStudyBarProApplication.java
@@ -0,0 +1,12 @@
+package com.hi.HiStudyBarPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiStudyBarProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiStudyBarProApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hi/HiStudyBarPro/HiStudyBarProApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hi.HiStudyBarPro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HiStudyBarProApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
